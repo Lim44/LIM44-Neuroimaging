dbd616fd62fe48a1eca6b968667530d64f4753ed
Lim44 <lim44.inrad@gmail.com>
2017-09-01 18:34:01 -0300

    Update runMIST.m

--- MIST/runMIST.m
+++ MIST/runMIST.m
@@ -4,7 +4,7 @@
 % Author: Raymundo Machado de Azevedo Neto
 %         Paulo Rodrigo Bazan
 % Date Created: 22 aug 2017
-% Last Update: 25 aug 2017
+% Last Update: 01 sep 2017
 
 clear all
 close all
@@ -37,8 +37,10 @@ try
     %% Initilization prior to experiment start
     trial_per_block = [0 0]; % count how many trials the participant carried out in each experimental and control blocks. Each line is a block
     count_block = [0 0];
-    count_operation = [1 1 1];  % counter to keep track of how many operations were performed in each condition  
+    count_operation = [1 1 1];  % counter to keep track of how many operations were performed in each condition
     flag_correct = 0;
+    trial_length = params.initial_trial_length;
+    response = 0;
     
     %% Main Experiment
     
@@ -62,7 +64,7 @@ try
         
         % Select which condition to run on each block
         if isequal(params.blocks{b},'experiment')
-            time_out = 2; % Define it based on training later            
+            time_out = trial_length; % Define it based on training later
             col_exp = 1; % this variable helps alocate values on correct column
             
             % If it is at the first trial, pctg_correct_flag starts at
@@ -71,13 +73,13 @@ try
                 pctg_correct_flag = params.pctg_corect;
             else
                 pctg_correct_flag = pctg_correct;
-            end    
+            end
             
             % Update block count
             count_block(1) = count_block(1) + 1;
             
         elseif isequal(params.blocks{b},'control')
-            time_out = []; 
+            time_out = [];
             pctg_correct_flag = [];
             col_exp = 2; % this variable helps alocate values on correct column
             
@@ -92,6 +94,9 @@ try
         % Loop through all trials whithin a block
         while params.time_block >= timer_block % Should think of better names for these two variables
             
+            % Restart ITI
+            ITI = params.ITI;
+            
             % Select operation from list
             if ~isequal(params.blocks{b},'rest')
                 operation = params.operations{sum(count_operation)-2};
@@ -99,21 +104,23 @@ try
                 operation = '+';
             end
             
-            % Select ITI for the current trial
-            ITI = 1;                        
-            
             % Execute one trial
-            [response(count_operation(col_exp), col_exp),time,log] = executeMISTtrial(operation,time_out,pctg_correct_flag,params);
+            [response(count_operation(col_exp), col_exp),time,log] = executeMISTtrial(operation,time_out,pctg_correct_flag,params,response,count_operation);
+            
+            % Check trial length and update ITI (increase or decrease)
+            % accordingly
+            if time > trial_length
+                ITI = ITI - (time - trial_length);
+            elseif time < trial_length
+                ITI = ITI + (trial_length - time);
+            end
+            
+            % Update trial_length
             
             % Update logs
             
             
             % Show to experimenter in which trial of which block
-            
-            % Run the Inter-Trial Interval if it is not rest
-            if ~isequal(params.blocks{b},'rest')
-                [~,~,~] = executeMISTtrial('+',ITI,pctg_correct_flag,params);
-            end                        
             
             % Update number of trials per block
             if isequal(params.blocks{b},'experiment')
@@ -139,7 +146,7 @@ try
                 
                 if any(response(:,1)) && flag_correct == 0
                     flag_correct = 1;
-                end                
+                end
                 
                 % Update feedback bar
                 if flag_correct == 1
@@ -149,10 +156,19 @@ try
                 
                 % Update operation counter
                 count_operation(1) = count_operation(1) + 1;
-            elseif isequal(params.blocks{b},'control')                                
+            elseif isequal(params.blocks{b},'control')
                 
                 % Update operation counter
                 count_operation(2) = count_operation(2) + 1;
+                
+                pctg_correct_flag = [];
+            end
+            
+            % Run the Inter-Trial Interval if it is not rest
+            if ~isequal(params.blocks{b},'rest')
+                before_ITI = GetSecs;
+                [~,~,~] = executeMISTtrial('+',ITI,pctg_correct_flag,params);
+                ITI_timing = GetSecs - before_ITI;
             end
             
             % Press ESC to abort the experiment
@@ -170,8 +186,11 @@ try
             end
             
             % Update block timer
-            timer_block = GetSecs - block_start;                        
+            timer_block = GetSecs - block_start;
         end                
+        
+        % Check how long was the block
+        block_length(b) = timer_block;
         
         % Check block timing
         check_block_time2 = GetSecs - check_block_time;
@@ -193,7 +212,7 @@ try
     % Acknowledgment screen
     Screen('TextSize', params.ptb.w.id, 50);
     Screen('TextFont',params.ptb.w.id, 'Arial');
-
+    
     Text = ['Esta parte do experimento acabou. \n\n' '\n\n Aguarde mais instruções.'];
     DrawFormattedText(params.ptb.w.id, Text, 'center', 'center',0);
     Screen('Flip',params.ptb.w.id);
@@ -205,15 +224,15 @@ try
     Screen('CloseAll');
     
     %% Creating folder to save participant's data and changing directory\
-%     if ~exist([],'dir')
-%         mkdir([])
-%     end
-%     oldfolder=cd([]);
+    %     if ~exist([],'dir')
+    %         mkdir([])
+    %     end
+    %     oldfolder=cd([]);
     
     %% Saving output
     
     
-%     cd(oldfolder)
+    %     cd(oldfolder)
 catch ME
     % ---------- Error Handling ----------
     % If there is an error in our code, we will end up here.
